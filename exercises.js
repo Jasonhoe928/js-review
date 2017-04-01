3dc7023d215424b23d97852656e65d1ada89da74
Jason <Jason@Colins-MacBook-Pro.local>
2017-03-31 17:02:10 -1000

    Completed 1

--- exercises.js
+++ exercises.js
@@ -8,7 +8,8 @@ Now, unleash your coding beast!
 
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
-
+var yourName;
+console.log(yourName);
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear. Please assign the actual data for each variable.
 Console log each variable.*/
